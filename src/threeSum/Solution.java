4425c8c2473980acc47eb80d9456a344e5bcabea
liutaotao <lttzzlll@gmail.com>
2017-07-19 23:37:31 +0800

    first commit

---
+++ src/threeSum/Solution.java
@@ -0,0 +1,10 @@
+/**
+ * Created by 10609 on 2017/7/19.
+ */
+package threeSum;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
