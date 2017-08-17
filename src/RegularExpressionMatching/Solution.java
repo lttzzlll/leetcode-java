fcf116b8ddb92a08f12f35eb2a6bfa36317ea95a
liutaotao <lttzzlll@icloud.com>
2017-08-17 20:58:53 +0800

    Regular Expression Using Dynamic Programming reviewed

--- src/RegularExpressionMatching/Solution.java
+++ src/RegularExpressionMatching/Solution.java
@@ -112,39 +112,66 @@ public class Solution {
 //        return isMatch(s, 0, p, 0);
 //    }
 
+    /**
+     * code: the shorter, the better
+     *
+     * @param s input string
+     * @param p input Regular Expression Pattern
+     * @return match or not, true of false
+     */
+//    public boolean isMatch(String s, String p) {
+//        if (s == null || p == null) {
+//            return false;
+//        }
+//        int slen = s.length();
+//        int plen = p.length();
+//        boolean[][] dp = new boolean[slen + 1][plen + 1];
+//        dp[0][0] = true;
+//        for (int i = 0; i < plen; i++) {
+//            if (p.charAt(i) == '*' && dp[0][i - 1]) {
+//                dp[0][i + 1] = true;
+//            }
+//        }
+//
+//        for (int i = 0; i < slen; i++) {
+//            for (int j = 0; j < plen; j++) {
+//                // determine dp[i][j]
+//                if (p.charAt(j) == s.charAt(i) || p.charAt(j) == '.') {
+//                    dp[i + 1][j + 1] = dp[i][j];
+//                }
+//                // determine dp[i + 1][j + 1]
+//                if (p.charAt(j) == '*') {
+//                    if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.') {
+//                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
+//                    } else {
+//                        dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1]);
+//                    }
+//                }
+//            }
+//        }
+//        return dp[slen][plen];
+//    }
     public boolean isMatch(String s, String p) {
-        if (s == null || p == null) {
-            return false;
-        }
-        int slen = s.length();
-        int plen = p.length();
-        boolean[][] dp = new boolean[slen + 1][plen + 1];
-        dp[0][0] = true;
-        for (int i = 0; i < plen; i++) {
-            if (p.charAt(i) == '*' && dp[0][i - 1]) {
-                dp[0][i + 1] = true;
-            }
-        }
-
-        for (int i = 0; i < slen; i++) {
-            for (int j = 0; j < plen; j++) {
-                if (p.charAt(j) == '.') {
-                    dp[i + 1][j + 1] = dp[i][j];
-                }
-                if (p.charAt(j) == s.charAt(i)) {
-                    dp[i + 1][j + 1] = dp[i][j];
-                }
-                if (p.charAt(j) == '*') {
-                    if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.') {
-                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
-                    } else {
-                        dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1]);
-                    }
-                }
-            }
-        }
-        return dp[slen][plen];
+        return isMatch(s.toCharArray(), p.toCharArray());
     }
+
+    public boolean isMatch(char[] s, char[] p) {
+        int m = s.length, n = p.length;
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 0; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 2] ||
+                            (i > 0 && (s[i - 1] == p[j - 2] || p[j - 2] == '.') && dp[i - 1][j]);
+                } else {
+                    dp[i][j] = i > 0 && dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
     public static void main(String[] args) {
         String a = "aa";
         String b = "a";
