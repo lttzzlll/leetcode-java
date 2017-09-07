77a129f98959a271fc7ca0da2f1a578a12c332bd
liutaotao <lttzzlll@icloud.com>
2017-09-07 21:33:21 +0800

    save

--- src/BestTimetoBuyandSellStock2/Solution.java
+++ src/BestTimetoBuyandSellStock2/Solution.java
@@ -7,11 +7,6 @@ public class Solution {
 
 
     public int maxProfit(int[] prices) {
-        int[] sum = new int[prices.length];
-        int res = prices[0]; sum[0] = prices[0];
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] <)
-        }
     }
 
     public static void main(String[] args) {
